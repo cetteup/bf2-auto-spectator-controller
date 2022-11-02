97f28e08d5b6ee6ecaa784b038263ddc14a4de33
cetteup <me@cetteup.com>
2022-11-02 23:59:55 +0100

    fix: Handle exception during controller launch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Controller from './controller';
+import logger from './logger';
 
-(async () => {
-    const controller = new Controller();
-    await controller.run();
-})();
+const controller = new Controller();
+controller.run()
+    .catch((e: any) => logger.error('Failed to launch controller:', e));
 
 
